898bfd99199248e08b9d101b90ac861ecf49572a
Quy Nguyen <quy.ybu@gmail.com>
2022-03-14 09:15:50 -0500

    miniprofie-updatee

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { signOut, useSession } from "next-auth/react"
 
 function MiniProfile() {
+  const { data: session } = useSession(); 
+
+  console.log(session);
+
   return (
     <div className='flex items-center justify-between mt-14 ml-10'>
     <img className='w-16 h-16 rounded-full border p-[2px]' src='https://www.looper.com/img/gallery/fans-agree-this-is-the-worst-character-in-demon-slayer/intro-1629224623.webp' alt='' />
 
     <div className='flex-1 mx-4'>
-    <h2 className='font-bold'>quy.ybu</h2>
+    <h2 className='font-bold'>{session?.user?.username}</h2>
     <h3 className='text-sm text-gray-400'> Welcome to Instagram 2.0</h3>
     </div>
 
-    <button className='text-blue-400 text-sm font-semibold'>Sign out</button>
+    <button onClick={signOut} className='text-blue-400 text-sm font-semibold'>Sign out</button>
     
     </div>
   )
