43961750b794d1c96e49a816d470beb1d174ff97
Quy Nguyen <quy.ybu@gmail.com>
2022-03-17 09:38:03 -0500

    like donee

--- components/Post.js
+++ components/Post.js
@@ -10,13 +10,17 @@ import{
 import{HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 import { db } from "../firebase";
 import Moment from 'react-moment';
+import { useSession } from 'next-auth/react';
 
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp, setDoc, doc, deleteDoc } from 'firebase/firestore';
 
 
 function Post({id, username, userImg, img, caption}) {
   const {data: session } = useSession();
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const[hasLiked, setHasLiked] = useState(false);
 
   useEffect(
     () => 
@@ -27,8 +31,31 @@ function Post({id, username, userImg, img, caption}) {
         ),
         (snapshot) => setComments(snapshot.docs)
         ), 
-        [db]
-        );
+        [db, id]
+  );
+  useEffect(() => onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) => setLikes(snapshot.docs)
+  ), 
+  [db, id]
+  );
+
+  useEffect(
+    () => 
+    setHasLiked(
+      likes.findIndex((like)=> like.id === session?.user?.uid) !== -1
+    ),
+   [likes]
+   );
+
+  const likePost = async () => {
+    if(hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid));
+    }else {
+      await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+        username: session.user.username,
+      });
+    }
+  };
+
 
   const sendComment = async (e) => {
     e.preventDefault();
@@ -59,7 +86,13 @@ function Post({id, username, userImg, img, caption}) {
       {session && (
         <div className= "flex justify-between px-4 pt-4" >
         <div className='flex space-x-4'>
-          <HeartIcon className='btn' />
+        {
+          hasLiked ? (
+            <HeartIconFilled onClick={likePost} className='btn text-red-500' />
+          ) : (
+            <HeartIcon onClick={likePost} className='btn' />
+          )
+        }
           <ChatIcon className='btn'/>
           <PaperAirplaneIcon className='btn'/>
         </div>
@@ -71,6 +104,9 @@ function Post({id, username, userImg, img, caption}) {
 
       {/*caption*/}
       <p className='p-5 truncate'>
+      {likes.length > 0 && (
+          <p className='font-bold mb-1'>{likes.length} likes</p>
+      )}
       <span className="font-bold mr-1 ">{username} </span> 
       {caption}
       </p>
@@ -119,8 +155,5 @@ function Post({id, username, userImg, img, caption}) {
     </div>
   )
 }
-import { useSession } from 'next-auth/react';
-import { async } from '@firebase/util';
-import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore';
 
 export default Post
